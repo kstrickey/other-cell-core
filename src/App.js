c7cc3f4c66999dff464c81a43cc5672328cc21c7
Kevin Trickey <trickeykevin@gmail.com>
2024-08-10 09:16:54 -0700

    updated icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
         
         <p>
