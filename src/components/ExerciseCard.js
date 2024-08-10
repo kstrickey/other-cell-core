21d15cf5cfbb9d62ecf7429b82136dcee68d90c8
Kevin Trickey <trickeykevin@gmail.com>
2024-08-10 12:18:19 -0700

    added icon support

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { EXERCISES } from '../libs/exercises';
 
+export default function ExerciseCard({ exerciseKey, className='', ...props }) {
 
-export default function({ exerciseKey }) {
-
+  if (!EXERCISES[exerciseKey]) return null;
 
   return (
-    <Card>
-      {exerciseKey}
+    <Card className={className + ' px-3 py-1 clickable zoomable-mid'} key={'exercise-card-' + exerciseKey} {...props}>
+      <div className='d-flex'>
+        {EXERCISES[exerciseKey].iconPath && <img src={EXERCISES[exerciseKey].iconPath} width={60} alt={EXERCISES[exerciseKey].displayName} />}
+        <div className='h4 my-auto ms-3 text-start'>
+          {EXERCISES[exerciseKey].displayName}
+        </div>
+      </div>
     </Card>
   );
 
--- src/libs/exercises.js
+++ src/libs/exercises.js
@@ -1,8 +1,8 @@
-import exercises from './exercises.json';
+import exercise_data from './exercises.json';
 
 
 
 
-export const DEFAULT_EXERCISE_LIST = Object.keys(exercises);
+export const DEFAULT_EXERCISE_LIST = Object.keys(exercise_data);
 
-
+export const EXERCISES = exercise_data;   // have this step because I was thinking we could do some post translational modifications in this js file
