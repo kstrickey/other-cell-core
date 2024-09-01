89051f082d9501b0b6e426e2af39f0608a2aa0ea
Jennifer Hu <jenniferferhu@gmail.com>
2024-08-31 21:44:26 -0700

    now it's in its own file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import { DEFAULT_EXERCISE_LIST } from '../libs/exercises';
 import ExerciseCard from '../components/ExerciseCard';
+import Stopwatch from '../components/Stopwatch';
 
 
 function getShuffledExerciseList() {
@@ -12,53 +13,6 @@ function getShuffledExerciseList() {
       .concat([DEFAULT_EXERCISE_LIST[Math.floor(Math.random() * DEFAULT_EXERCISE_LIST.length)]])   // add an extra exercise (sneaky sneaky!)
       .map(val => ({ val, sort: Math.random() }))  // randomize order
       .sort((a, b) => a.sort - b.sort).map(({ val }) => val)
-}
-
-function Stopwatch() {
-  // state to store time
-  const [time, setTime] = useState(0);
-
-  // state to check stopwatch running or not
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    let intervalId;
-    if (isRunning) {
-      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
-      intervalId = setInterval(() => setTime(time + 1), 10);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, time]);
-
-  // Minutes calculation
-  const minutes = Math.floor((time % 360000) / 6000);
-  // Seconds calculation
-  const seconds = Math.floor((time % 6000) / 100);
-  // Method to start and stop timer
-  const startAndStop = () => {
-    setIsRunning(!isRunning);
-  };
-
-  // Method to reset timer back to 0
-  const reset = () => {
-    setTime(0);
-  };
-  return (
-    <div className="stopwatch-container">
-      <p className="stopwatch-time">
-        {minutes.toString().padStart(2, "0")}:
-        {seconds.toString().padStart(2, "0")}
-      </p>
-      <div className="stopwatch-buttons">
-        <button className="stopwatch-button" onClick={startAndStop}>
-          {isRunning ? "Stop" : "Start"}
-        </button>
-        <button className="stopwatch-button" onClick={reset}>
-          Reset
-        </button>
-      </div>
-    </div>
-  );
 }
 
 export default function Home() {
